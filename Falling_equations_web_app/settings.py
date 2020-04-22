bbb8e24e043a904feaad944297ac1e70b85cb6dd
Marcin-99 <MarcinAszyk@o2.pl>
2020-04-22 23:11:01 +0200

    Added password reset functionality (with env variables)

--- Falling_equations_web_app/settings.py
+++ Falling_equations_web_app/settings.py
@@ -146,3 +146,12 @@ AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+"""Sending email."""
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
