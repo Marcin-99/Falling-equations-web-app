d7af26881c09aab8f16ffa9a739ced928ff70d15
Marcin-99 <MarcinAszyk@o2.pl>
2020-04-26 22:25:06 +0200

    Updated settings

--- Falling_equations_web_app/settings.py
+++ Falling_equations_web_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('FALLING_EQUATIONS_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('FALLING_EQUATIONS_DEBUG')
 
-ALLOWED_HOSTS = ['https://falling-equations.herokuapp.com/']
+ALLOWED_HOSTS = ['falling-equations.herokuapp.com']
 
 
 # Application definition
