48c317dd18aa0ce3929ebcafd226c0fe1eecfeac
Marcin-99 <MarcinAszyk@o2.pl>
2020-04-26 22:37:01 +0200

    Updated settings

--- Falling_equations_web_app/settings.py
+++ Falling_equations_web_app/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -158,3 +159,4 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('GMAIL_PASSWORD')
 
 
+django_heroku.settings(locals())
